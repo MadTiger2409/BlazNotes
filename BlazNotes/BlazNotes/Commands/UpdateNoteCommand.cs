92a62e7a4b1882e3d66a76a808da558e27af3742
Michał B <cyberwolf_96@outlook.com>
2019-05-28 13:55:50 +0200

    Redesign EditNote page with EditForm.

---
+++ BlazNotes/BlazNotes/Commands/UpdateNoteCommand.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using BlazNotes.Models;
+
+namespace BlazNotes.Commands
+{
+    public class UpdateNoteCommand
+    {
+        [Required(ErrorMessage = "Title is required.", AllowEmptyStrings = false)]
+        [MinLength(1, ErrorMessage = "Title must be at least 1 character long.")]
+        [MaxLength(50, ErrorMessage = "Title can't be longer than 50 characters.")]
+        [Editable(true)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is required.", AllowEmptyStrings = false)]
+        [MinLength(3, ErrorMessage = "Description must be at least 3 characters long.")]
+        [MaxLength(500, ErrorMessage = "Description can't be longer than 500 characters.")]
+        [Editable(true)]
+        public string Description { get; set; }
+
+        public UpdateNoteCommand() { }
+
+        public UpdateNoteCommand(Note note)
+        {
+            Title = note.Title;
+            Description = note.Description;
+        }
+    }
+}

af4705957bf1d42054e424f49f838edddba9c500
Michał B <cyberwolf_96@outlook.com>
2019-05-28 09:43:57 +0200

    Little changes in errors messages.

--- BlazNotes/BlazNotes/Commands/CreateNoteCommand.cs
+++ BlazNotes/BlazNotes/Commands/CreateNoteCommand.cs
@@ -4,15 +4,15 @@ namespace BlazNotes.Commands
 {
     public class CreateNoteCommand
     {
-        [Required]
-        [MinLength(1)]
-        [MaxLength(50)]
+        [Required(ErrorMessage = "Title is required.", AllowEmptyStrings = false)]
+        [MinLength(1, ErrorMessage = "Title must be at least 1 character long.")]
+        [MaxLength(50, ErrorMessage = "Title can't be longer than 50 characters.")]
         [Editable(true)]
         public string Title { get; set; }
 
-        [Required]
-        [MinLength(3)]
-        [MaxLength(500)]
+        [Required(ErrorMessage = "Description is required.", AllowEmptyStrings = false)]
+        [MinLength(3, ErrorMessage = "Description must be at least 3 characters long.")]
+        [MaxLength(500, ErrorMessage = "Description can't be longer than 500 characters.")]
         [Editable(true)]
         public string Description { get; set; }
     }

2e65868b287bbc463e1c40ab30bcd6b232181179
Michał B <cyberwolf_96@outlook.com>
2019-06-22 17:43:28 +0200

    Working on theme

---
+++ BlazNotes/BlazNotes/Helpers/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+namespace BlazNotes.Helpers.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string UppercaseFirst(this string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+
+            char[] a = s.ToCharArray();
+            a[0] = char.ToUpper(a[0]);
+            return new string(a);
+        }
+    }
+}
